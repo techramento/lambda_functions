2b625225ee2e46f480852add1d8b9238a660b925
Derek Lindahl <derek.lindahl@gmail.com>
2017-03-23 16:15:26 -0700

    Move Dotenv call further up the stack

--- calendar-aggregator/handler.js
+++ calendar-aggregator/handler.js
@@ -1,4 +1,7 @@
 const aggregator = require('./aggregator')
+const dotenv = require('dotenv')
+
+dotenv.load()
 
 module.exports.calendars = (event, context, callback) =>
   aggregator(event, callback)
