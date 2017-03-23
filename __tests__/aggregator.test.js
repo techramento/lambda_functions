9912d3a3b77fd9de8e06890b1adcddacf282bd6a
Derek Lindahl <derek.lindahl@gmail.com>
2017-03-23 00:46:53 -0700

    Add initial test suite for calendar aggregator

---
+++ __tests__/aggregator.test.js
@@ -0,0 +1,14 @@
+const aggregator = require('../aggregator')
+
+const event = Object.freeze({})
+
+test('calendar aggregation', (done) =>
+  aggregator(event, (err, response) => {
+    expect(err).toBeNull()
+    expect(response.statusCode).toBe(200)
+    const body = JSON.parse(response.body)
+    expect(body.input).not.toBeNull()
+    expect(body.data.length).toBeGreaterThan(0)
+    expect(body.data[0].meetup).not.toBeNull()
+    done()
+  }))
